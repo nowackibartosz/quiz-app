efa70f926259d6116f7a0eed6bb64b4394d601d4
Bartosz Nowacki <nowackib@protonmail.com>
2022-06-02 15:35:54 +0200

    build questions page

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,5 +1,26 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
 const Questions = () => {
-  return <div>Questions</div>;
+  return (
+    <Box>
+      <Typography variant="h4">Questions 1</Typography>
+      <Typography vmt={5}>this is the questions</Typography>
+      <Box mt={2}>
+        <Button variant="contained">Answer1</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained">Answer2</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained">Answer3</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained">Answer4</Button>
+      </Box>
+      <Box mt={5}>Score: 2/6</Box>
+    </Box>
+  );
 };
 
 export default Questions;
