25c80836f67614c1ba22537719e97311616baa05
Bartosz Nowacki <nowackib@protonmail.com>
2022-06-02 15:51:51 +0200

    creat reducer and store

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,7 +1,21 @@
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useSelector } from "react-redux";
+import useAxios from "../hooks/useAxios";
 
 const Questions = () => {
+  const {
+    question_category,
+
+    question_difficulty,
+    question_type,
+    amount_of_question,
+  } = useSelector((state) => state);
+  console.log(amount_of_question);
+  let apiUrl = `/api.php?amount=10`;
+
+  const { response, loading } = useAxios({ url: apiUrl });
+  console.log(response);
   return (
     <Box>
       <Typography variant="h4">Questions 1</Typography>
